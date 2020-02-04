bd444845694db303a6b6f10cf6a8b9e0e651a24d
saiyenman <goku72013@gmail.com>
2020-02-04 20:10:19 +0100

    added models + service

--- app/src/main/java/com/intellitech/creches/models/Group.kt
+++ app/src/main/java/com/intellitech/creches/models/Group.kt
@@ -2,7 +2,7 @@ package com.intellitech.creches.models
 
 data class Group(
     val events: List<Event>,
-    val groupProfile: GourpProfileX,
+    val groupProfile: GroupProfile,
     val meals: List<Meal>,
     val other: List<Other>,
     val section: Section,
--- app/src/main/java/com/intellitech/creches/models/GroupProfile.kt
+++ app/src/main/java/com/intellitech/creches/models/GroupProfile.kt
@@ -1,6 +1,6 @@
 package com.intellitech.creches.models
 
-data class GourpProfile(
+data class GroupProfile(
     val Type: String,
     val educatorProfile: EducatorProfile,
     val id: String,

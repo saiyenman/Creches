9004412138c60209052ca1a77bd39ffd8664cb11
saiyenman <goku72013@gmail.com>
2020-02-21 21:50:56 +0100

    Each parent sees his kids group event

--- app/src/main/java/com/intellitech/creches/services/FirebaseMessagingService.kt
+++ app/src/main/java/com/intellitech/creches/services/FirebaseMessagingService.kt
@@ -61,7 +61,7 @@ class FirebaseMessagingService : FirebaseMessagingService() {
         // message, here is where that should be initiated. See sendNotification method below.
     }
 
-    /**
+    /***
      * Called if InstanceID token is updated. This may occur if the security of
      * the previous token had been compromised. Note that this is called when the InstanceID token
      * is initially generated so this is where you would retrieve the token.
